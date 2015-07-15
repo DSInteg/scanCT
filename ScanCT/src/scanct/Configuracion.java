80426b03fd8f84088e18a12e1be6f893c0705ddb
Elizabeth H <cyberelizabeth@hotmail.com>
2015-07-15 13:23:51 -0500

    Se realizaron cambios en la configuración Remota
    Debido a que se correría en otro equipo en una red distinta se configuro
    de manera diferente el proyecto

--- ScanCT/src/scanct/Configuracion.java
+++ ScanCT/src/scanct/Configuracion.java
@@ -8,12 +8,12 @@ package scanct;
  */
 public class Configuracion {
     //Carpetas
-    final String carpetaRemota="\\\\THINKPADL430\\";
+   // final String carpetaRemota="\\\\THINKPADL430\\";
     final String carpetaCT = this.carpetaRemota + "aceptadosCT\\";
-    //final String carpetaRemota="\\\\ServExpedientes\\";
+    final String carpetaRemota="c:\\escaneos\\";
     //final String carpetaLocal="/home/eli-vaio/Desktop/dsinteg/escaneos";
     //Base de Datos local
-    final String USUARIO="root";
-    final String PASSWORD="";
+    final String USUARIO="curp";
+    final String PASSWORD="curpuset";
     final String DATABASE_URL="jdbc:mysql://127.0.0.1/curp_uset";
 }
